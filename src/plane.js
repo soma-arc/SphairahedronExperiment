b37b7c5eebd71a22428c8d7e558c39fb1d5c263a
soma_arc <somaarcr@gmail.com>
2017-10-28 23:58:37 +0900

    Implement inversion

--- src/plane.js
+++ src/plane.js
@@ -1,4 +1,5 @@
 import Vec3 from './vector3d.js';
+import Sphere from './sphere.js';
 
 export default class Plane {
     /**
@@ -22,5 +23,41 @@ export default class Plane {
             'p2': [this.p2.x, this.p2.y, this.p2.z],
             'p3': [this.p3.x, this.p3.y, this.p3.z]
         };
+    }
+
+    /**
+     *
+     * @param {Plane} plane
+     * @returns {Plane}
+     */
+    invertOnPlane(plane) {
+        const dp = Vec3.dot(this.normal, plane.normal);
+        const nNormal = plane.normal.sub(this.normal.scale(2 * dp)).normalize();
+        return new Plane(this.invertOnPoint(plane.p1),
+                         this.invertOnPoint(plane.p2),
+                         this.invertOnPoint(plane.p3),
+                         nNormal);
+    }
+
+    /**
+     * 
+     * @param {Sphere} sphere
+     * @returns {Sphere} 
+     */
+    invertOnSphere(sphere) {
+        const nc = this.invertOnPoint(sphere.center);
+        return new Sphere(nc.x, nc.y, nc.z, sphere.r);
+    }
+
+    /**
+     * 
+     * @param {Vec3} p
+     * @returns {Vec3} 
+     */
+    invertOnPoint(p) {
+        let pos = p.sub(this.p1);
+        const dp = Vec3.dot(this.normal, pos);
+        pos = pos.sub(this.normal.scale(2 * dp));
+        return pos.add(this.p1);
     }
 }
